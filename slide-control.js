de9f05b461355c0e3b5e2d69ae9890216e716293
Ari Shtein <ashtein120@gmail.com>
2022-03-21 08:33:53 -0400

    files added, basic structures provided

---
+++ slide-control.js
@@ -0,0 +1,10 @@
+
+/* 
+This is the basic structure. Call the function on 'play game' pressed and then
+check all answers and build the storyline
+*/
+function play_game() {
+  document.querySelector('button.yes1').addEventListener('click', function () {
+
+  })
+}
